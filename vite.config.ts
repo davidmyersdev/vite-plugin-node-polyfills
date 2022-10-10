5240c17763c8e26b7c34a119f99be733de5427b8
David R. Myers <davidrmyersii@gmail.com>
2022-10-09 21:02:17 -0400

    Fix indentation in config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       formats: [],
     },
     rollupOptions: {
-    external: [/^node:.*$/],
+      external: [/^node:.*$/],
       output: [
         {
           esModule: true,
