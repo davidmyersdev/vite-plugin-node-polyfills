e74bb318e3043d6ff02872370971a6c149f9e2c6
David R. Myers <davidrmyersii@gmail.com>
2023-04-11 09:33:47 -0400

    Add fs to the test script

--- test/src/main.ts
+++ test/src/main.ts
@@ -1,6 +1,7 @@
 import { Buffer } from 'node:buffer'
 import { resolve } from 'node:path'
 import * as process from 'node:process'
+import fs from 'node:fs'
 import { fetch } from 'ohmyfetch'
 
 console.log(fetch)
@@ -9,3 +10,4 @@ console.log(process.env)
 console.log(globalThis.Array)
 console.log(Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]))
 console.log(Array)
+console.log(fs)
