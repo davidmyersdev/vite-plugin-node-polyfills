bb7ab7cbd261e29d9d8d983d22465a69174eeca6
David R. Myers <davidrmyersii@gmail.com>
2023-09-10 19:00:40 -0400

    Add test for lodash-es Uint8Array conflict

--- examples/vanilla/src/main.ts
+++ examples/vanilla/src/main.ts
@@ -2,7 +2,16 @@ import { Buffer } from 'node:buffer'
 import { resolve } from 'node:path'
 import * as process from 'node:process'
 import fs from 'node:fs'
+import { cloneDeep } from 'lodash-es'
 import { fetch } from 'ohmyfetch'
+
+const something = {
+  some: true,
+  else: 1,
+  inner: {
+    buffer: [0, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 7],
+  },
+}
 
 console.log(fetch)
 console.log(resolve('.'))
@@ -13,3 +22,4 @@ console.log(Buffer.from([0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff]).readBi
 console.log(Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]))
 console.log(Array)
 console.log(fs)
+console.log(cloneDeep(something))
