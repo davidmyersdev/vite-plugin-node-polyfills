0631ec6b6fe4aafb6ac78406b0f8870acf56d2c3
David R. Myers <davidrmyersii@gmail.com>
2023-11-28 23:47:59 -0500

    Add CI workflow

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,4 +2,14 @@ module.exports = {
   extends: [
     'artisan',
   ],
+  overrides: [
+    {
+      files: [
+        './.github/**/*.yml',
+      ],
+      rules: {
+        'yml/no-empty-mapping-value': 'off',
+      },
+    },
+  ],
 }
