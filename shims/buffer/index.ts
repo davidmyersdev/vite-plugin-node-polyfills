c87b6b4e7aacdbe8a5ae831c7cddbb64d49cb72a
David R. Myers <davidrmyersii@gmail.com>
2024-02-10 15:31:48 -0500

    Make sure all named `buffer` modules are exported

--- shims/buffer/index.ts
+++ shims/buffer/index.ts
@@ -1,5 +1,43 @@
-// eslint-disable-next-line unicorn/prefer-node-protocol, n/no-deprecated-api
-import { Buffer, SlowBuffer, kMaxLength } from 'buffer'
+import {
+  Blob,
+  BlobOptions,
+  Buffer,
+  File,
+  FileOptions,
+  INSPECT_MAX_BYTES,
+  // eslint-disable-next-line n/no-deprecated-api
+  SlowBuffer,
+  TranscodeEncoding,
+  atob,
+  btoa,
+  constants,
+  isAscii,
+  isUtf8,
+  kMaxLength,
+  kStringMaxLength,
+  resolveObjectURL,
+  transcode,
+// eslint-disable-next-line unicorn/prefer-node-protocol
+} from 'buffer'
 
-export { Buffer, SlowBuffer, kMaxLength }
+export {
+  Blob,
+  BlobOptions,
+  Buffer,
+  File,
+  FileOptions,
+  INSPECT_MAX_BYTES,
+  SlowBuffer,
+  TranscodeEncoding,
+  atob,
+  btoa,
+  constants,
+  isAscii,
+  isUtf8,
+  kMaxLength,
+  kStringMaxLength,
+  resolveObjectURL,
+  transcode,
+}
+
 export default Buffer
