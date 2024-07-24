c57bd2c5aba774428bca66b1acc98f4a2bee6844
Avinash Thakur <97439243+80avin-sw@users.noreply.github.com>
2024-07-25 03:03:43 +0530

    update global, process if they are already defined.
    Fixes https://github.com/davidmyersdev/vite-plugin-node-polyfills/issues/83

--- src/index.ts
+++ src/index.ts
@@ -94,11 +94,11 @@ const globalShimBanners = {
   ],
   global: [
     `import __global_polyfill from 'vite-plugin-node-polyfills/shims/global'`,
-    `globalThis.global = globalThis.global || __global_polyfill`,
+    `globalThis.global = { ...__global_polyfill, ...globalThis.global }`,
   ],
   process: [
     `import __process_polyfill from 'vite-plugin-node-polyfills/shims/process'`,
-    `globalThis.process = globalThis.process || __process_polyfill`,
+    `globalThis.process = { ...__process_polyfill, ...globalThis.process }`,
   ],
 }
 
